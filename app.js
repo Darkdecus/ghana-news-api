47aa5eb4bf1373944b715697a483f24ac8f36efa
Darkdecus <pkaabudu@gmail.com>
2021-10-09 19:20:06 +0000

    refresh cache for puppeteer build

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV === "development" && app.use(require("morgan")("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const router = require("./routers/index.router");
-// app.use("/", router);
 app.use("/", require("./routers/index.router"));
 
 app.listen(process.env.PORT, () => {
